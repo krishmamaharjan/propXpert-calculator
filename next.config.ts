86a32ccb1bc6b8bf5b6b70b2ce927877071fa302
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-12-31 17:50:31 +0545

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
+import type { NextConfig } from "next";
 
 
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
 
+};
 module.exports = nextConfig;
